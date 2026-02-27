e0722b539b2aa677ce9ff8f5f2f927ae2fe52f7f
Charles Titus <ctitus@bnl.gov>
2026-02-27 14:15:14 -0500

    IOC built out and tested locally

--- tes_ioc/dastard_model.py
+++ tes_ioc/dastard_model.py
@@ -1,9 +1,10 @@
 import asyncio
 import zmq
 import zmq.asyncio
-
+import collections
 import json
 import itertools
+import time
 
 
 class DastardError(Exception):
@@ -15,7 +16,7 @@ class AsyncDastardModel:
     def __init__(self, host, port, config = {}):
         # Dastard ZMQ Sub
         self.context = zmq.asyncio.Context()
-        self.socket = zmq.asyncio.socket(zmq.SUB)
+        self.socket = self.context.socket(zmq.SUB)
         self.host = host
         self.baseport = port
         self.subport = port + 1
@@ -24,6 +25,7 @@ class AsyncDastardModel:
         self.socket.setsockopt_string(zmq.SUBSCRIBE, "")
         self.messages_seen = collections.Counter()
         self.cache = {}
+        self.channel_names = []
 
         # Dastard command
         self.config = config
@@ -35,14 +37,13 @@ class AsyncDastardModel:
     async def update_messages(self):
         while True:
             try:
-                topic, contents = await self.get_message()
+                topic, contents = asyncio.create_task(self.get_message())
                 
                 if topic is None:
                     break
-                self.cache[topic] = contents
             except Exception as e:
                 print(f"Error updating messages: {e}")
-                break        
+                break
         
     async def get_message(self):
         msg = await self.socket.recv_multipart()
@@ -52,8 +53,19 @@ class AsyncDastardModel:
             raise Exception(f"msg: `{msg}` should have two parts, but does not")
         topic = topic.decode()
         contents = json.loads(contents_str.decode())
+        if topic is not None:
+            self.cache[topic] = contents
         self.messages_seen[topic] += 1
         return topic, contents
+
+    async def wait_for_message_with_topic(self, target_topic: str, message_count: int, timeout=10.0):
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            if self.cache.get(target_topic) is not None:
+                if self.messages_seen[target_topic] >= message_count:
+                    return self.cache[target_topic]
+            await asyncio.sleep(0.2)
+        raise TimeoutError(f"Timeout waiting for message with topic {target_topic}")
 
     def get_message_with_topic(self, target_topic: str):
         return self.cache.get(target_topic)
@@ -80,6 +92,7 @@ class AsyncDastardModel:
             response = await reader.read(4096)
             writer.close()
             await writer.wait_closed()
+        return response
 
     async def send(self, method_name: str, params, verbose=True):
         # Needs a timeout and error!
@@ -106,4 +119,107 @@ class AsyncDastardModel:
         
         return response["result"]
 
-    async def 
+    async def request_status(self):
+        response = await self.send("SourceControl.SendAllStatus", "dummy")
+        return response
+
+    async def start_file(self, ljh22=None, off=None, path=None):
+        params = {"Request": "Start", "WriteLJH3": False}
+        params.update(self.config.get("writeControl", {}))
+
+        if ljh22 is not None:
+            params["WriteLJH22"] = ljh22
+        if off is not None:
+            params["WriteOFF"] = off
+        if path is not None:
+            params["Path"] = path
+
+        writing_count = self.messages_seen.get("WRITING", 0)
+        print("DEBUG: WriteControl params: ", params)
+        response = await self.send("SourceControl.WriteControl", params)
+
+        try:
+            contents = await self.wait_for_message_with_topic("WRITING", writing_count + 1)
+        except TimeoutError:
+            raise DastardError("Timeout waiting for WRITING message")
+        if not contents["Active"]:
+            raise DastardError(
+                f'Response from Dastard RPC should have contents["Active"]=True, but it does not\ncontents:\n{contents}'
+            )
+        self.off_filename = contents["FilenamePattern"] % ("chan1", "off")
+        return self.off_filename
+
+    async def stop_writing(self):
+        params = {"Request": "Stop"}
+        response = await self.send("SourceControl.WriteControl", params)
+        return response
+
+    async def set_experiment_state(self, state):
+        params = {"Label": state, "WaitForError": True}
+        response = await self.send("SourceControl.SetExperimentStateLabel", params)
+        return response
+
+    async def set_triggers(self, full_trigger_state):
+        response = await self.send("SourceControl.ConfigureTriggers", full_trigger_state)
+        return response
+
+    async def configure_record_lengths(self, npre=None, nsamp=None):
+        params = self.config.get("pulseLengths", {})
+        if nsamp is not None:
+            params["Nsamp"] = nsamp
+        if npre is not None:
+            params["Npre"] = npre
+        response = await self.send("SourceControl.ConfigurePulseLengths", params)
+        return response
+
+    async def set_pulse_trigger_all_chans(self, threshold=None, n_monotone=None):
+        config = {"ChannelIndices": self.get_channel_indices()}
+        config.update(self.config.get("pulseTrigger", {}))
+        if threshold is not None:
+            config["EdgeMultiLevel"] = threshold
+        if n_monotone is not None:
+            config["EdgeMultiVerifyNMonotone"] = n_monotone
+        response = await self.set_triggers(config)
+
+        #if self.config.get("use_bahama", False):
+        #    self.configure_bahama_pulses()
+        return response
+
+    async def set_noise_trigger_all_chans(self):
+        config = {
+            "ChannelIndices": self.get_channel_indices(),
+        }
+        config.update(self.config.get("noiseTrigger", {}))
+        response = await self.set_triggers(config)
+
+        #if self.config.get("use_bahama", False):
+        #    self.configure_bahama_noise()
+        return response
+
+    async def zero_all_triggers(self):
+        config = {
+            "ChannelIndices": self.get_channel_indices(),
+        }
+        response = await self.set_triggers(config)
+        return response
+
+    def get_n_channels(self):
+        return len(self.get_name_to_number_index())
+
+    def get_channel_indices(self):
+        return list(self.get_name_to_number_index().values())
+
+    # dastard channelNames go from chan1 to chanN and err1 to errN
+    # we need to map from channelName to channelIndex (0-2N-1)
+    def get_name_to_number_index(self):
+        nameNumberToIndex = {}
+        for i, name in enumerate(self.channel_names):
+            if not name.startswith("chan"):
+                continue
+            nameNumber = int(name[4:])
+            nameNumberToIndex[nameNumber] = i
+            # for now since we only use this with lancero sources, error for non-odd index
+            # if i % 2 != 1:
+            #     raise Exception(
+            #         "all fb channelIndices on a lancero source are odd, we shouldn't load projectors for even channelIndices")
+        return nameNumberToIndex
---
+++ tes_ioc/ioc.py
@@ -0,0 +1,210 @@
+import asyncio
+import os
+from caproto.server import (
+    PVGroup,
+    ioc_arg_parser,
+    pvproperty,
+    run,
+)
+from .dastard_model import AsyncDastardModel
+from caproto import ChannelType
+import numpy as np
+import tomllib
+
+class TESIOC(PVGroup):
+    """
+    An IOC to run a TES detector through Dastard
+    """
+    alive = pvproperty(value=False, dtype=bool, name="CONNECTED")
+    running = pvproperty(value=False, dtype=bool, name="RUNNING")
+    writing = pvproperty(value=False, dtype=bool, name="WRITING")
+    source = pvproperty(value="None", dtype=ChannelType.STRING, name="SOURCE")
+    filename = pvproperty(value="", dtype=ChannelType.STRING, name="FILENAME")
+    write_ljh = pvproperty(value=False, dtype=bool, name="WRITE_LJH")
+    write_off = pvproperty(value=False, dtype=bool, name="WRITE_OFF")
+    projectors = pvproperty(value=False, dtype=bool, name="PROJECTORS")
+    state = pvproperty(value="", dtype=ChannelType.STRING, name="STATE")
+    n_channels = pvproperty(value=0, dtype=int, name="NCHAN")
+    n_disabled = pvproperty(value=0, dtype=int, name="NDISABLED")
+    trigger_rate = pvproperty(value=0, dtype=int, name="TRIGGERRATE")
+
+    n_samples = pvproperty(value=0, dtype=int, name="NSAMPLES")
+    n_presamples = pvproperty(value=0, dtype=int, name="NPRESAMPLES")
+
+    file_start = pvproperty(value=0, dtype=int, name="FILE_START")
+    file_end = pvproperty(value=0, dtype=int, name="FILE_END")
+    scan_start = pvproperty(value=0, dtype=int, name="SCAN_START")
+    scan_end = pvproperty(value=0, dtype=int, name="SCAN_END")
+    acquire = pvproperty(value=0, dtype=float, name="ACQUIRE")
+
+    set_pulse_trigger = pvproperty(value=0, dtype=int, name="SET_PULSE_TRIGGER")
+    set_noise_trigger = pvproperty(value=0, dtype=int, name="SET_NOISE_TRIGGER")
+
+    def __init__(self, *args, config={},**kwargs):
+        super().__init__(*args, **kwargs)
+        self.dastard_model = AsyncDastardModel(host="dastard", port=5500, config=config)
+        self._off_filename = ""
+        self._channel_names = []
+
+    async def _handle_message(self, topic, contents):
+        """
+        Handle ZMQ messages from Dastard. Mirrors handle_message in
+        tes_scan_server.dastard_client.DastardClient.
+        """
+        if topic not in ["TRIGGERRATE", "EXTERNALTRIGGER", "ALIVE"]:
+            print(f"DEBUG: Topic: {topic}, Contents: {contents}")
+        if topic == "ALIVE":
+            running = contents.get("Running", False)
+            if self.running.value != running:
+                await self.running.write(running)
+
+        elif topic == "STATUS":
+            print("DEBUG: Got Status Message")
+            if self.running.value:
+                print("DEBUG: Running, updating statuses")
+                source = contents.get("SourceName", "None")
+                nsamples = contents.get("Nsamples", 0)
+                npresamp = contents.get("Npresamp", 0)
+                nchannels = contents.get("Nchannels", 0)
+                projectors = contents.get("ChannelsWithProjectors", [])
+                has_projectors = bool(projectors)
+
+                tasks = []
+                if self.source.value != source:
+                    tasks.append(self.source.write(source))
+                
+                if self.projectors.value != has_projectors:
+                    tasks.append(self.projectors.write(has_projectors))
+                if self.n_samples.value != nsamples:
+                    tasks.append(self.n_samples.write(nsamples))
+                if self.n_presamples.value != npresamp:
+                    tasks.append(self.n_presamples.write(npresamp))
+                if self.n_channels.value != nchannels:
+                    tasks.append(self.n_channels.write(nchannels))
+                await asyncio.gather(*tasks)
+
+        elif topic == "CHANNELNAMES":
+            if self.dastard_model.channel_names != contents:
+                self.dastard_model.channel_names = contents
+
+        elif topic == "WRITING":
+            writing = contents.get("Active", False)
+            if self.writing.value != writing:
+                await self.writing.write(writing)
+            if writing:
+                filename = contents["FilenamePattern"] % ("chan1", "off")
+                if self._off_filename != filename:
+                    self._off_filename = filename
+                    await self.filename.write(filename)
+            elif self._off_filename != "":
+                self._off_filename = ""
+                await self.filename.write("")
+
+        elif topic == "TRIGGER":
+            trigger_types = [
+                "AutoTrigger",
+                "LevelTrigger",
+                "EdgeTrigger",
+                "EdgeMulti",
+                "EdgeMultiNoise",
+            ]
+            n_chans = len(self._channel_names)
+            for item in contents:
+                if not any(item.get(t, False) for t in trigger_types):
+                    if len(item["ChannelIndices"]) < n_chans:
+                        await self.n_disabled.write(len(item["ChannelIndices"]))
+        elif topic == "TRIGGERRATE":
+            counts = np.sum(contents.get("CountsSeen", [0]))
+            duration = contents.get("Duration", 0) / 1e9
+            await self.trigger_rate.write(int(counts/duration))
+
+    @alive.startup
+    async def alive(self, instance, async_lib):
+        try:
+            topic, contents = await asyncio.wait_for(
+                self.dastard_model.get_message(),
+                timeout=10.0,
+            )
+            await self._handle_message(topic, contents)
+            alive = True
+        except asyncio.TimeoutError:
+            alive = False
+        except Exception:
+            alive = False
+        await instance.write(alive)
+
+        if alive:
+            await self.dastard_model.request_status()
+
+        while True:
+            try:
+                topic, contents = await asyncio.wait_for(
+                    self.dastard_model.get_message(),
+                    timeout=10.0,
+                )
+                await self._handle_message(topic, contents)
+                alive = True
+            except asyncio.TimeoutError:
+                alive = False
+            except Exception:
+                alive = False
+            await instance.write(alive)
+    
+    @set_pulse_trigger.putter
+    async def set_pulse_trigger(self, instance, value):
+        await instance.write(1, verify_value=False)
+        await self.dastard_model.configure_record_lengths()
+        await self.dastard_model.zero_all_triggers()
+        await self.dastard_model.set_pulse_trigger_all_chans()
+        return 0
+
+    @set_noise_trigger.putter
+    async def set_noise_trigger(self, instance, value):
+        await instance.write(1, verify_value=False)
+        await self.dastard_model.configure_record_lengths()
+        await self.dastard_model.zero_all_triggers()
+        await self.dastard_model.set_noise_trigger_all_chans()
+        return 0
+
+    @write_ljh.startup
+    async def write_ljh(self, instance, async_lib):
+        write_ljh = self.dastard_model.config.get("writeControl", {}).get("WriteLJH22", False)
+        await self.write_ljh.write(write_ljh)
+
+    @write_off.startup
+    async def write_off(self, instance, async_lib):
+        write_off = self.dastard_model.config.get("writeControl", {}).get("WriteOFF", False)
+        await self.write_off.write(write_off)
+
+    @file_start.putter
+    async def file_start(self, instance, value):
+        await instance.write(1, verify_value=False)
+        try:
+            write_ljh = self.write_ljh.value == "On"
+            write_off = self.write_off.value == "On"
+            filename = await self.dastard_model.start_file(ljh22=write_ljh, off=write_off)
+            await self.filename.write(filename)
+            return 0
+        except Exception as e:
+            await self.filename.write("")
+            return 0
+
+    @file_end.putter
+    async def file_end(self, instance, value):
+        await instance.write(1, verify_value=False)
+        await self.dastard_model.stop_writing()
+        return 0
+
+if __name__ == "__main__":
+    ioc_options, run_options = ioc_arg_parser(
+        default_prefix="XF:07ID-ES{{UCAL}}:",
+        desc="TES IOC",
+        supported_async_libs=("asyncio",),
+    )
+
+    config_path = os.path.join(os.path.dirname(__file__), "config.toml")
+    with open(config_path, "rb") as f:
+        config = tomllib.load(f)
+    config = config.get("dastard", {})
+    ioc = TESIOC(config=config, **ioc_options)
+    run(ioc.pvdb, **run_options)
